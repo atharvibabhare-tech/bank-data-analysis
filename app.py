09e7acf9ca94f14ef8291299a0ac2f0945bdffbc
Atharvi Babhare <tanujababhare@gmail.com>
2026-02-11 14:40:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,8 +19,7 @@ st.markdown("Interactive analysis of customer subscription behavior")
 # -----------------------------
 @st.cache_data
 def load_data():
-    return pd.read_csv("data/Bank.csv")
-
+    return pd.read_csv("bank_data/Bank.csv")
 try:
     df = load_data()
 except:
